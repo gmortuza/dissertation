b91b1bd92789464ebd40522f80509296905f0ca3
Golam Mortuza <golammdmortuza@u.boisestate.edu>
2021-04-11 01:33:34 -0600

    frames saved into splitted section

--- generate_training_data/generate_data.py
+++ generate_training_data/generate_data.py
@@ -45,10 +45,10 @@ class GenerateData():
         self.distributed_photon = None  # tensor in shape(num_binding_event, total frames)
         # Initially the movie is just a random noise
         self.movie = get_noise(self.config)  # Tensor of shape (num_of_frames, height, width)
-        self.frame_wise_noise = self.movie.mean((1, 2))  # Tensor of shape (num_of_frames)
+        self.frame_wise_noise = self.movie.mean((1, 2)).share_memory_()  # Tensor of shape (num_of_frames)
         # self.movie = torch.zeros((self.config.frames, self.config.image_size, self.config.image_size),
         #                          device=self.config.device, dtype=torch.float64)
-        self.drifts = get_drift(self.config)  # tensor of shape (num_of_frames, 2)
+        self.drifts = get_drift(self.config).share_memory_()  # tensor of shape (num_of_frames, 2)
         # This will be used to sample from multivariate distribution
         # This ground truth is only for visualization
         # Normally we don't need this ground truth for training purpose
@@ -56,20 +56,31 @@ class GenerateData():
         # TODO: export training example supporting neural network training
         self.available_cpu_core = int(self.config.num_of_process)
 
+    def get_frame_range(self):
+        num_of_frame_in_single_file = self.config.frames // self.config.split_into
+        frame_range = []
+        for i in range(self.config.split_into):
+            single_range = [i * num_of_frame_in_single_file, (i+1) * num_of_frame_in_single_file]
+            frame_range.append(single_range)
+        frame_range[-1][-1] = self.config.frames
+        return frame_range
+
     def generate(self):
-        self.binding_site_position = generate_binding_site_position(self.config)
+        self.binding_site_position = generate_binding_site_position(self.config).share_memory_()
         self.binding_site_position_distribution = get_binding_site_position_distribution(self.binding_site_position, self.config)
         self.num_of_binding_site = self.binding_site_position.shape[1]
 
         self.distribute_photons()
-        self.convert_into_image()
+        frame_range = self.get_frame_range()
+        for frame_start, frame_end in frame_range:
+            self.convert_into_image(frame_start, frame_end)
         # self.save_image()
 
     @show_execution_time
     def distribute_photons(self):
         self.config.logger.info("Distributing photons")
         self.distributed_photon = torch.zeros((self.num_of_binding_site, self.config.frames),
-                                              device=self.config.device)
+                                              device=self.config.device).share_memory_()
         # Setting up method for multiprocessing
         photon_distributor = partial(distribute_photons_single_binding_site, config=self.config,
                                      num_of_binding_site=self.num_of_binding_site)
@@ -84,46 +95,45 @@ class GenerateData():
             self.distributed_photon[site_id] = photon
 
     @show_execution_time
-    def convert_into_image(self):
+    def convert_into_image(self, frame_start, frame_end):
         self.config.logger.info("Converting into Images")
         p_convert_frame = partial(convert_frame, config=self.config, drifts=self.drifts,
                                   distributed_photon=self.distributed_photon, frame_wise_noise=self.frame_wise_noise,
                                   binding_site_position_distribution=self.binding_site_position_distribution)
         if self.config.num_of_process > 1:
             with Pool(self.available_cpu_core) as pool:
-                frame_details = pool.map(p_convert_frame, torch.arange(self.config.frames))
-            # with concurrent.futures.ProcessPoolExecutor(max_workers=self.available_cpu_core) as executor:
-            #     frame_details = executor.map(p_convert_frame, torch.arange(self.config.frames))
+                frame_details = pool.map(p_convert_frame, torch.arange(frame_start, frame_end))
         else:
-            frame_details = map(p_convert_frame, torch.arange(self.config.frames))
+            frame_details = map(p_convert_frame, torch.arange(frame_start, frame_end))
 
         # we save images in tensor for training purpose
-        ground_truth = self.save_frames_in_tensor(frame_details)
+        ground_truth = self.save_frames_in_tensor(frame_details, frame_start, frame_end)
         if self.config.save_for_picasso:
-            self.save_frames_in_hdf5(ground_truth)
+            self.save_frames_in_hdf5(ground_truth, frame_start, frame_end)
 
-    def save_frames_in_tensor(self, frame_details):
+    def save_frames_in_tensor(self, frame_details, frame_start, frame_end):
         # concatnenating tensor in a loop might be slower
         # So we are creating a large tensor
         # frame_num, x, y, x_mean, y_mean, x_drifted, y_drifted, photons, s_x, s_y, noise
-        combined_ground_truth = torch.zeros((self.config.frames * self.config.max_number_of_emitter_per_frame, 11), device=self.config.device)
-        current_num_of_emitter = 0
-        for frame_id, frame, gt_infos in tqdm(frame_details, desc="Distributing photons", total=self.config.frames, disable=self.config.progress_bar_disable):
 
+        combined_ground_truth = torch.zeros(((frame_end-frame_start) * self.config.max_number_of_emitter_per_frame, 11), device=self.config.device).share_memory_()
+        current_num_of_emitter = 0
+        for frame_id, frame, gt_infos in tqdm(frame_details, desc="Distributing photons", total=frame_end - frame_start, disable=self.config.progress_bar_disable):
             self.movie[frame_id, :, :] += frame
             emitter_to_keep = min(len(gt_infos), self.config.max_number_of_emitter_per_frame)
             combined_ground_truth[current_num_of_emitter: current_num_of_emitter+emitter_to_keep, :] = gt_infos[:emitter_to_keep, :]
             current_num_of_emitter += emitter_to_keep
 
         combined_ground_truth = combined_ground_truth[: current_num_of_emitter, :]
-        torch.save(combined_ground_truth, self.config.output_file + "_ground_truth.pl")
-        torch.save(self.movie, self.config.output_file + ".pl")
+
+        torch.save(combined_ground_truth, self.config.output_file + f"_ground_truth_{frame_start + 1}_{frame_end}.pl")
+        torch.save(self.movie[frame_start: frame_end], self.config.output_file + f"_{frame_start+1}_{frame_end}.pl")
         return combined_ground_truth
 
     @show_execution_time
-    def save_frames_in_hdf5(self, ground_truth):
+    def save_frames_in_hdf5(self, ground_truth, frame_start, frame_end):
         # First save the movie
-        self.movie.cpu().numpy().tofile(self.config.output_file + ".raw")
+        self.movie[frame_start: frame_end].cpu().numpy().tofile(self.config.output_file + f"_{frame_start+1}_{frame_end}.raw")
         ground_truth = ground_truth.cpu().numpy()
         # frame_num, x, y, x_mean, y_mean, x_drifted, y_drifted, photons, s_x, s_y, noise
         gt_without_drift = np.rec.array(
@@ -176,14 +186,14 @@ class GenerateData():
                                 f"\nFrames: {self.config.frames}\n" \
                                 f"Height: {self.config.image_size}\n" \
                                 f"Width: {self.config.image_size}"
-        with h5py.File(self.config.output_file + "_gt_without_drift.hdf5", "w") as locs_file:
+        with h5py.File(self.config.output_file + f"_{frame_start+1}_{frame_end}_gt_without_drift.hdf5", "w") as locs_file:
             locs_file.create_dataset("locs", data=gt_without_drift)
-            with open(self.config.output_file + "_gt_without_drift.yaml", "w") as yaml_file:
+            with open(self.config.output_file + f"_{frame_start + 1}_{frame_end}_gt_without_drift.yaml", "w") as yaml_file:
                 yaml_file.write(content_for_yaml_file)
 
-        with h5py.File(self.config.output_file + "_gt_with_drift.hdf5", "w") as locs_file:
+        with h5py.File(self.config.output_file + f"_{frame_start + 1}_{frame_end}_gt_with_drift.hdf5", "w") as locs_file:
             locs_file.create_dataset("locs", data=gt_with_drift)
-            with open(self.config.output_file + "_gt_with_drift.yaml", "w") as yaml_file:
+            with open(self.config.output_file + f"_{frame_start + 1}_{frame_end}_gt_with_drift.yaml", "w") as yaml_file:
                 yaml_file.write(content_for_yaml_file)
 
 
