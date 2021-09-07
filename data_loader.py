3a67ed981f9fbfca0fd28aff814ef846252c6e8e
Golam Mortuza <golammdmortuza@u.boisestate.edu>
2021-09-07 02:08:59 -0600

    Save individual data to avoid scaling up the image everytime

--- data_loader.py
+++ data_loader.py
@@ -3,74 +3,68 @@ import os
 
 import torch
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
 from read_config import Config
 from generate_target import generate_target_from_path
 
 
 class SMLMDataset(Dataset):
-    def __init__(self, data_set_dir, config, type_='train'):
-        gt_file_names = glob.glob(f"{data_set_dir}/*_gt.pl")
+    def __init__(self, dataset_dir, config, type_='train'):
+
         self.config = config
         self.type_ = type_
-        if self.type_ == 'train':
-            self.data, self.target = self.get_data_and_target(gt_file_names)
-        else:
-            self.data = self.get_data_and_target(gt_file_names)
+        self.dataset_dir = dataset_dir
+        self.total_data = self._upsample_images()
 
-    def get_data_and_target(self, gt_file_names: list):
-        data = None
-        target = None
-        for single_gt_file in gt_file_names:
-            # TODO: Find a way of getting the input other than this
-            data_file_name = single_gt_file.replace("_gt", "")
-            single_data = torch.load(data_file_name)
-            single_data_shape = single_data.shape
-            single_data = single_data.reshape(single_data_shape[0], 1, single_data_shape[1], single_data_shape[2])
-            # there will be no target during test
-            if self.type_ != 'test':
-                single_target = generate_target_from_path(single_gt_file, self.config, target="points")
-            # Normalize target here so that we can change the datatype to avoid memory crash
-            # single_target = single_target / single_target.norm()
-            # single_target = single_target
-            if data is None:
-                data = single_data
-                if self.type_ != 'test':
-                    target = single_target
-            else:
-                data = torch.cat((data, single_data), dim=0)
-                if self.type_ != 'test':
-                    target = torch.cat((target, single_target), dim=0)
-        if self.type_ != 'test':
-            return data, target
-        return data
+    def _upsample_images(self):
+        file_names = glob.glob(f"{self.dataset_dir}/data_*_gt.pl")
+        # If there are already upsampled images we will return the number of images
+        upsampled_file_names = glob.glob(f"{self.dataset_dir}/up_{self.config.output_resolution}_*")
+        if len(upsampled_file_names) > 0:
+            return len(upsampled_file_names)
+        else:
+            total = 0
+            normalize_factor = 20000.
+            upsample_fn = torch.nn.Upsample(scale_factor=self.config.output_resolution, mode='bilinear', align_corners=True)
+            for file_name in sorted(file_names):
+                start = int(file_name.split('_')[-3]) - 1
+                input_ = torch.load(file_name.replace('_gt', ''), ).unsqueeze(1) / normalize_factor
+                label_ = torch.load(file_name)
+                label_[:, 7] /= normalize_factor
+                for idx, single_input in tqdm(enumerate(input_, start), total=input_.shape[0], desc="Upsampling the data individual",
+                              disable=self.config.progress_bar_disable, leave=False):
+                    single_input_upsampled = upsample_fn(single_input.unsqueeze(0))
+                    if self.type_ == 'test':
+                        combine_training = single_input_upsampled[0]
+                    else:
+                        single_label = label_[label_[:, 0] == idx]
+                        single_label_upsampled = self._get_image_from_point(single_label)
+                        combine_training = torch.cat((single_input_upsampled, single_label_upsampled), dim=0)
+                    f_name = f"{self.dataset_dir}/up_{self.config.output_resolution}_{idx}.pl"
+                    torch.save(combine_training, f_name)
+                    total += 1
+            return total
 
     def _get_image_from_point(self, point: torch.Tensor) -> torch.Tensor:
         # points --> [x, y, photons]
         high_res_image_size = self.config.image_size * self.config.output_resolution
         high_res_movie = torch.zeros((high_res_image_size, high_res_image_size), device=self.config.device)
         # TODO: remove this for loop and vectorize this
-        for blinker in point[point[:, 2] > 0.]:
-            mu = torch.round(blinker[[0, 1]] * self.config.output_resolution).int()
-            high_res_movie[mu[1]][mu[0]] += blinker[2]
-        return high_res_movie.unsqueeze(0)
+        for blinker in point[point[:, 7] > 0.]:
+            mu = torch.round(blinker[[1, 2]] * self.config.output_resolution).int()
+            high_res_movie[mu[1]][mu[0]] += blinker[7]
+        return high_res_movie.unsqueeze(0).unsqueeze(0)
 
     def __len__(self):
-        return self.data.shape[0]
+        return self.total_data
 
     def __getitem__(self, index: int):
-        x: torch.Tensor = self.data[index]
-        # Normalize the data
-        # TODO: change 50000 to maximum assigned photons
-        x = x / 50000.
-        # Upscale the input
-        x = torch.nn.Upsample(scale_factor=self.config.output_resolution, mode="bilinear")(x.unsqueeze(0))[0]
-        if self.type_ == 'train':
-            y: torch.Tensor = self._get_image_from_point(self.target[index])
-            # convert points into images
-            y = y / 50000.
-            return x, y
-        return x
+        f_name = f"{self.dataset_dir}/up_{self.config.output_resolution}_{index}.pl"
+        data = torch.load(f_name)
+        if data.shape[0] == 1:  # it's the test directory
+            return data[0]
+        return data[0], data[1]
 
 
 def fetch_data_loader(config: Config, shuffle: bool = True, type_: str = 'train'):
