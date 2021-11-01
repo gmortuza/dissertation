51e1c7195f4da2ffabfb25ee13649e24bbd412d1
Golam Mortuza <golammdmortuza@u.boisestate.edu>
2021-11-01 15:48:13 -0600

    data genration from borah

--- test_train.py
+++
@@ -1,8 +0,0 @@
-
-from models.srgan.train import train_evaluation
-from read_config import Config
-from torch.utils.tensorboard import SummaryWriter
-
-config = Config('config.yaml')
-config.tensor_board_writer = SummaryWriter(config.tensorflow_log_dir)
-train_evaluation()
