0e05a7d5f69e0f60a84661eaa1704ffe934346a2
Golam Mortuza <golammdmortuza@u.boisestate.edu>
2021-11-21 01:49:14 -0700

    BORAH - data generation

--- read_config.py
+++ read_config.py
@@ -46,7 +46,7 @@ class Config:
         self.log_level = 'error'  # disable console
         self.neptune_mode = 'async'  # always enable neptune
         self.progress_bar_disable = True
-        self.input_dir = '/bsuscratch/gmortuza/simulated_data'
+        self.input_dir = '/bsuscratch/gmortuza/simulated_data_497_old'
         self.output_dir = 'outputs_' + datetime.now().strftime("%d_%m_%H_%M_%S")
         self.load_checkpoint = False
         self.save_model_after_each_epoch = 5
@@ -55,7 +55,7 @@ class Config:
         self.test_split = 0.0
         self.device = 'cuda:0'
         self.total_frames = 200000
-        self.total_origami = 100
+        self.total_origami = 50
         self.split_into = 20
 
         repo = Repo(os.path.dirname(os.path.abspath(__file__)))
@@ -133,6 +133,7 @@ class Config:
             "upsample_method": 'transposed',
             "output_directory": self.output_dir,
             "weight_decay": self.weight_decay,
+            "input_dir": self.input_dir,
         }
         return neptune_run
 
