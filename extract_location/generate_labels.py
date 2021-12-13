6c8b054bc5779887ecd2fdb6d125c28e40a61380
Golam Mortuza <golammdmortuza@u.boisestate.edu>
2021-12-13 10:29:16 -0700

    POS: label extraction done

--- extract_location/generate_labels.py
+++ extract_location/generate_labels.py
@@ -1,8 +1,10 @@
 import os
 
 import glob
-import torch
 
+import pickle
+import torch
+import torch.nn.functional as F
 from read_config import Config
 
 
@@ -24,7 +26,7 @@ def if_intersect(x_start, x_end, y_start, y_end, points):
 def extract_label_from_single_frame(frame, gts):
     patches = []
     patches_gt = []
-    scale = 1.0
+    scale = 16.0
     points = []
     for point in gts:
         # extract the position from gt
@@ -48,20 +50,20 @@ def extract_label_from_single_frame(frame, gts):
             points[intersect_point] = (x_start, x_end, y_start, y_end)
             # Update existing points
             for label in patches_gt[intersect_point]:
-                label[1] = (label[-2] - x_start) / (x_end - x_start)
-                label[0] = (label[-1] - y_start) / (y_end - y_start)
+                label[1] = (label[-4] - x_start) / (x_end - x_start)
+                label[0] = (label[-3] - y_start) / (y_end - y_start)
             patches[intersect_point] = frame[x_start: x_end, y_start: y_end]
             # Add the current label in the gts
             x = (x_mean - x_start) / (x_end - x_start)
             y = (y_mean - y_start) / (y_end - y_start)
-            label = [y, x, float(point[7] / 20000.), float(point[8]), float(point[9]), x_mean, y_mean]
+            label = [y, x, float(point[7] / 20000.), float(point[8]), float(point[9]), x_mean, y_mean, x_start, y_start]
             patches_gt[intersect_point].append(label)
         else:
             points.append((x_start, x_end, y_start, y_end))
             patch = frame[x_start: x_end, y_start: y_end]
             x = (x_mean - x_start) / (x_end - x_start)
             y = (y_mean - y_start) / (y_end - y_start)
-            label = [y, x, float(point[7] / 20000.), float(point[8]), float(point[9]), x_mean, y_mean]
+            label = [y, x, float(point[7] / 20000.), float(point[8]), float(point[9]), x_mean, y_mean, x_start, y_start]
             patches.append(patch)
             patches_gt.append([label])
     return patches, patches_gt
@@ -69,6 +71,8 @@ def extract_label_from_single_frame(frame, gts):
 
 def extract_label_from_folder(folder, config):
     file_names = glob.glob(f"{folder}/data_*_gt.pl")
+    save_folder = os.path.join(folder, 'points')
+    os.makedirs(save_folder, exist_ok=True)
     for file_name in file_names:
         patches = []
         locations = []
@@ -77,21 +81,26 @@ def extract_label_from_folder(folder, config):
         data = torch.load(file_name.replace('_gt', '_' + str(config.resolution_slap[-1])), )
         # for each frame extract it's labels
         for frame_id, frame in enumerate(data, start=start):
-            # if frame_id != 49:
-            #     continue
             # get gt
             gt = gts[gts[:, 0] == frame_id]
             patch, location = extract_label_from_single_frame(frame, gt)
-            patches.extend(patch)
-            locations.extend(location)
-            max_ = 0
-            for patch in patches:
-                if patch.shape[0] > 10 or patch.shape[1] > 10:
-                    max_ = max(patch.shape[0], patch.shape[1])
-                    print(patch.shape)
+            # make all patches the same size
+            # patch = F.pad(patch, (0, 0, 0, 0, 32, 32), mode='constant', value=0)
+            for idx, (p, loc) in enumerate(zip(patch, location)):
+                pad = ((20 - p.shape[1]) // 2, (20 - p.shape[1]) // 2, (20 - p.shape[0]) // 2, (20 - p.shape[0]) // 2)
+                single_patch = F.pad(p, pad, mode='constant', value=0)
+                # create label
+                label = [1, loc[0][0], loc[0][1], loc[0][3], loc[0][4], loc[0][2]]
+                if len(loc) > 1:
+                    label.extend([1, loc[1][0], loc[1][1], loc[1][3], loc[1][4], loc[1][2]])
+                else:
+                    label.extend([0, 0, 0, 0, 0, 0])
 
+                label = torch.Tensor(label)
 
-        print(max_)
+                file_name = os.path.join(save_folder, f"p_{frame_id}_{idx}.pl")
+                with open(file_name, 'wb') as f:
+                    pickle.dump([single_patch, label], f)
 
 
 def main(config):
